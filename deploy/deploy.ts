66b95c608a7370a40e592f0e9c685e99224dd00b
danielwongd <vqjneljtw60175@outlook.com>
2025-12-24 14:40:34 +0800

    Refine deployment script for lending contracts

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -4,14 +4,30 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
+  const { ethers } = hre;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedCUSDT = await deploy("ConfidentialUSDT", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  const deployedTrustlessLend = await deploy("TrustlessLend", {
+    from: deployer,
+    args: [deployedCUSDT.address],
+    log: true,
+  });
+
+  const signer = await ethers.getSigner(deployer);
+  const cusdtContract = await ethers.getContractAt("ConfidentialUSDT", deployedCUSDT.address, signer);
+  const currentMinter = await cusdtContract.minter();
+  if (currentMinter.toLowerCase() !== deployedTrustlessLend.address.toLowerCase()) {
+    const tx = await cusdtContract.setMinter(deployedTrustlessLend.address);
+    await tx.wait();
+  }
+
+  console.log(`ConfidentialUSDT contract: `, deployedCUSDT.address);
+  console.log(`TrustlessLend contract: `, deployedTrustlessLend.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_trustless_lend"; // id required to prevent reexecution
+func.tags = ["TrustlessLend"];
